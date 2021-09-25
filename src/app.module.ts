697581ecb7d517caa2c566a5bf528fa2bfd7860d
Evgen Kozenkov <klon19892@gmail.com>
2021-09-25 14:29:12 +0300

    docs: update app module and install orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { MovieModule } from './movies/movies.module'
+import { MovieService } from './movies/services/movies.service'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '6Hjof8TT',
+      database: 'kinoteka',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    MovieModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
