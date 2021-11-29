29e0f7bcd203e277e724235dc97eb03fd00ac4c7
Evgen Kozenkov <klon19892@gmail.com>
2021-11-29 12:05:53 +0300

    Update modules

--- src/auth/admin.guard.ts
+++ src/auth/admin.guard.ts
@@ -10,7 +10,7 @@ export class AdminAuthGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
     const { user } = request;
-    if (!user.isAdmin === true) {
+    if (user.isAdmin !== true) {
       throw new ForbiddenException();
     }
     return true;
